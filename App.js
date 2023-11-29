22f68fb7f11f19a40ae5544689b77224a563dd19
Prassu1811 <146934475+Prassu1811@users.noreply.github.com>
2023-11-29 16:44:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var jsxHeading = (
+    <div>
+<h1 id="jsxHeading">Heading from JSX!!</h1>
+<h1 id="jsxHeading2">Heading from JSX!!</h1>
+<h1>Hello, world!</h1>
+      <p>This is a paragraph.</p>
+      <ul>
+        <li>marrages</li>
+        <li>functions</li>
+        <li>parties</li>
+      </ul>
+      <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+      <br/>
+      <br/>
+      <button>click me</button>
+    </div>
+   
+);
+var htmlRoot = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(htmlRoot);
+rootReact.render(jsxHeading);
